2a705f4ffdda345d31e3f9a50ca24f14b63be868
pmclay <michaelpage1616@gmail.com>
2020-08-26 21:45:59 -0600

    UTs written for Trie.

--- adts/test_prefix_tree.go
+++ adts/test_prefix_tree.go
@@ -8,10 +8,201 @@ import (
 // to be a freshly instantiated object
 
 func testPrefixTreeAddEmpty(pt PrefixTree, t *testing.T) {
+	obj := NewElement("abcde")
+	pt.Add(obj)
+
+	s := pt.Size()
+	if s != 1 {
+		t.Errorf("Adding an item to empty prefix tree caused; size=%d", s)
+	}
+
+	if !pt.Contains(obj.String()) {
+		t.Errorf("Item was not added to empty prefix tree")
+	}
 }
 
 func testPrefixTreeAddNonEmpty(pt PrefixTree, t *testing.T) {
+	obj1 := NewElement("fghi")
+	obj2 := NewElement("abcde")
+	pt.Add(obj1)
+	pt.Add(obj2)
+
+	s := pt.Size()
+	if s != 2 {
+		t.Errorf("Adding an item to prefix tree caused; size=%d", s)
+	}
+
+	if !pt.Contains(obj2.String()) {
+		t.Errorf("Item was not added")
+	}
+
+	if !pt.Contains(obj1.String()) {
+		t.Errorf("Item was not added")
+	}
 }
 
 func testPrefixTreeAddOnExisting(pt PrefixTree, t *testing.T) {
+	obj1 := NewElement("abcde")
+	obj2 := NewElement("abcde")
+	pt.Add(obj1)
+	pt.Add(obj2)
+
+	s := pt.Size()
+	if s != 2 {
+		t.Errorf("Adding an item to prefix tree caused; size=%d", s)
+	}
+
+	if !pt.Contains(obj2.String()) {
+		t.Errorf("Item was not added")
+	}
+
+	if !pt.Contains(obj1.String()) {
+		t.Errorf("Item was not added")
+	}
+}
+
+func testPrefixTreeAddPrefix(pt PrefixTree, t *testing.T) {
+	obj1 := NewElement("abcde")
+	obj2 := NewElement("abc")
+	pt.Add(obj1)
+	pt.Add(obj2)
+
+	s := pt.Size()
+	if s != 2 {
+		t.Errorf("Adding an item to prefix tree caused; size=%d", s)
+	}
+
+	if !pt.Contains(obj2.String()) {
+		t.Errorf("Item which is a prefix of an existing item was not added")
+	}
+
+	if !pt.Contains(obj1.String()) {
+		t.Errorf("Item was not added")
+	}
+}
+
+func testPrefixTreeAddToPrefix(pt PrefixTree, t *testing.T) {
+	obj1 := NewElement("abc")
+	obj2 := NewElement("abcde")
+	pt.Add(obj1)
+	pt.Add(obj2)
+
+	s := pt.Size()
+	if s != 2 {
+		t.Errorf("Adding an item to prefix tree caused; size=%d", s)
+	}
+
+	if !pt.Contains(obj2.String()) {
+		t.Errorf("Item which extends an existing item was not added")
+	}
+
+	if !pt.Contains(obj1.String()) {
+		t.Errorf("Item was not added")
+	}
+}
+
+func testPrefixTreeContainsEmpty(pt PrefixTree, t *testing.T) {
+	obj := NewElement("abcde")
+
+	if pt.Contains(obj.String()) {
+		t.Errorf("Item was found in empty prefix tree")
+	}
+}
+
+func testPrefixTreeContainsDoesNotExist(pt PrefixTree, t *testing.T) {
+	obj1 := NewElement("fghi")
+	obj2 := NewElement("abcde")
+	pt.Add(obj1)
+
+	if pt.Contains(obj2.String()) {
+		t.Errorf("Non-existing item was found")
+	}
+}
+
+func testPrefixTreeContainsExistsAlone(pt PrefixTree, t *testing.T) {
+	obj := NewElement("abcde")
+	pt.Add(obj)
+
+	if !pt.Contains(obj.String()) {
+		t.Errorf("Existing item was not found")
+	}
+}
+
+func testPrefixTreeContainsExists(pt PrefixTree, t *testing.T) {
+	obj1 := NewElement("abcde")
+	obj2 := NewElement("fghij")
+	pt.Add(obj1)
+	pt.Add(obj2)
+
+	if !pt.Contains(obj2.String()) {
+		t.Errorf("Existing item was not found")
+	}
+}
+
+func testPrefixTreeContainsPrefixDoesNotExist(pt PrefixTree, t *testing.T) {
+	obj1 := NewElement("abcde")
+	obj2 := NewElement("abc")
+	pt.Add(obj1)
+
+	if pt.Contains(obj2.String()) {
+		t.Errorf("Non-existing prefix of item was found")
+	}
+}
+
+func testPrefixTreeContainsPrefixExists(pt PrefixTree, t *testing.T) {
+	obj1 := NewElement("abcde")
+	obj2 := NewElement("abc")
+	pt.Add(obj1)
+	pt.Add(obj2)
+
+	if !pt.Contains(obj2.String()) {
+		t.Errorf("Existing prefix of item was not found")
+	}
+}
+
+func testPrefixTreeContainsPrefixExistsForPrefix(pt PrefixTree, t *testing.T) {
+	obj1 := NewElement("abc")
+	obj2 := NewElement("abcde")
+	obj3 := NewElement("abcd")
+	pt.Add(obj1)
+	pt.Add(obj2)
+	pt.Add(obj3)
+
+	if !pt.Contains(obj1.String()) {
+		t.Errorf("Existing prefix of item was not found")
+	}
+}
+
+func testPrefixTreeContainsPostfixDoesNotExist(pt PrefixTree, t *testing.T) {
+	obj1 := NewElement("abc")
+	obj2 := NewElement("abcde")
+	pt.Add(obj1)
+
+	if pt.Contains(obj2.String()) {
+		t.Errorf("Non-existing item was found")
+	}
+}
+
+func testPrefixTreeContainsPostfixExists(pt PrefixTree, t *testing.T) {
+	obj1 := NewElement("abc")
+	obj2 := NewElement("abcde")
+	pt.Add(obj1)
+	pt.Add(obj2)
+
+	if !pt.Contains(obj2.String()) {
+		t.Errorf("Existing item was not found")
+	}
+}
+
+func testPrefixTreeSize(pt PrefixTree, t *testing.T) {
+	i := 0
+	for ; i < 25; i++ {
+		if pt.Size() != i {
+			t.Errorf("A prefix tree with %d items had size=%d", i, pt.Size())
+		}
+	}
+
+	if pt.Size() != i {
+		t.Errorf("A prefix tree with %d items had size=%d", i, pt.Size())
+	}
 }
--- adts/trie_test.go
+++ adts/trie_test.go
@@ -18,3 +18,63 @@ func TestTrieAddOnExisting(t *testing.T) {
 	var pt PrefixTree = NewTrie()
 	testPrefixTreeAddEmpty(pt, t)
 }
+
+func TestTrieAddPrefix(t *testing.T) {
+	var pt PrefixTree = NewTrie()
+	testPrefixTreeAddPrefix(pt, t)
+}
+
+func TestTrieAddToPrefix(t *testing.T) {
+	var pt PrefixTree = NewTrie()
+	testPrefixTreeAddToPrefix(pt, t)
+}
+
+func TestTrieContainsEmpty(t *testing.T) {
+	var pt PrefixTree = NewTrie()
+	testPrefixTreeContainsEmpty(pt, t)
+}
+
+func TestTrieContainsDoesNotExist(t *testing.T) {
+	var pt PrefixTree = NewTrie()
+	testPrefixTreeContainsDoesNotExist(pt, t)
+}
+
+func TestTrieContainsExistsAlone(t *testing.T) {
+	var pt PrefixTree = NewTrie()
+	testPrefixTreeContainsExistsAlone(pt, t)
+}
+
+func TestTrieContainsExists(t *testing.T) {
+	var pt PrefixTree = NewTrie()
+	testPrefixTreeContainsExists(pt, t)
+}
+
+func TestTrieContainsPrefixDoesNotExist(t *testing.T) {
+	var pt PrefixTree = NewTrie()
+	testPrefixTreeContainsPrefixDoesNotExist(pt, t)
+}
+
+func TestTrieContainsPrefixExists(t *testing.T) {
+	var pt PrefixTree = NewTrie()
+	testPrefixTreeContainsPrefixExists(pt, t)
+}
+
+func TestTrieContainsPrefixExistsForPrefix(t *testing.T) {
+	var pt PrefixTree = NewTrie()
+	testPrefixTreeContainsPrefixExistsForPrefix(pt, t)
+}
+
+func TestTrieContainsPostfixDoesNotExist(t *testing.T) {
+	var pt PrefixTree = NewTrie()
+	testPrefixTreeContainsPostfixDoesNotExist(pt, t)
+}
+
+func TestTrieContainsPostfixExists(t *testing.T) {
+	var pt PrefixTree = NewTrie()
+	testPrefixTreeContainsPostfixExists(pt, t)
+}
+
+func TestTrieSize(t *testing.T) {
+	var pt PrefixTree = NewTrie()
+	testPrefixTreeSize(pt, t)
+}
